443915e680751a1a3c6133f4f4baed9f34bdd5a7
meladria <98776699+meladria@users.noreply.github.com>
2022-09-08 21:58:38 -0500

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -54,5 +54,5 @@ function redirectToRoomName(name)
 function logout() {
 localStorage.removeItem("user_name");
 localStorage.removeItem("room_name");
-    window.location = "kwitter.html";
+    window.location = "index.html";
 }
